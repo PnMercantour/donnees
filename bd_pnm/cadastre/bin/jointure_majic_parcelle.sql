76b7ca5f964875d8b756e0ea6c2d3cafeeda3188
EcMer <125668563+EcMerc@users.noreply.github.com>
2024-05-14 10:33:04 +0200

    Update jointure_majic_parcelle.sql

--- bd_pnm/cadastre/bin/jointure_majic_parcelle.sql
+++ bd_pnm/cadastre/bin/jointure_majic_parcelle.sql
@@ -2,16 +2,16 @@
 
 /*
 
-Pour les données 2023 issues d'Etalab et de datagouv
+Pour les donnÃ©es 2023 issues d'Etalab et de datagouv
 https://www.data.gouv.fr/fr/datasets/fichiers-des-locaux-et-des-parcelles-des-personnes-morales/
 
-Les numéros de parcelles permettant de faire la jointure entre le PCI et le fichier des personnes morales est structuré de la façon suivante
-|Département|Commune (lpad, '0',3)|prefixe (lpad, '0',3)|section|numéro plan(lpad, '0',4)|
+Les numÃ©ros de parcelles permettant de faire la jointure entre le PCI et le fichier des personnes morales est structurÃ© de la faÃ§on suivante
+|DÃ©partement|Commune (lpad, '0',3)|prefixe (lpad, '0',3)|section|numÃ©ro plan(lpad, '0',4)|
 |:--:|:--:|:--:|:--:|:--:
 |06|114|000|AA|0262||
 
-Ce qui se traduit par la requête suivante pour réaliser une jointure.
-(Par commodité, dans la table cadastre_personne_morale, le champs "id_parcelle" a été ajouté pour réaliser la jointure)
+Ce qui se traduit par la requÃªte suivante pour rÃ©aliser une jointure.
+(Par commoditÃ©, dans la table cadastre_personne_morale, le champs "id_parcelle" a Ã©tÃ© ajoutÃ© pour rÃ©aliser la jointure)
 */
 
 SELECT * from cadastre.cadastre_parcelle parcelle
@@ -19,5 +19,6 @@ JOIN cadastre.cadastre_personne_morale morale
 ON = parcelle.id = concat(
 	'0', morale.departement::varchar, 
 	lpad(morale.code_commune::varchar,3,'0') , 
-	lpad(replace(morale.prefixe::varchar,' ',''),3,  '0' ),"section",
+	lpad(replace(morale.prefixe::varchar,' ',''),3,  '0' ),
+	lpad("section"::varchar,2,'0'),
 	lpad(morale.plan::varchar, 4,'0'))  
